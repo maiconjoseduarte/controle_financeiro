e4047bebbae9510015ddeb4682b6f7694e1077a6
Maicon José Duarte <duartejosemaicon@gmail.com>
2019-01-16 21:30:45 -0200

    Ajustes no layout

---
+++ common/components/Icones.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: maicon
+ * Date: 23/09/18
+ * Time: 18:51
+ */
+namespace common\components;
+
+class Icones
+{
+    const VISUALIZAR = 'glyphicon glyphicon-eye-open';
+    const EDITAR = 'glyphicon glyphicon-pencil';
+    const EXCLUIR = 'glyphicon glyphicon-trash';
+    const CADASTRAR = 'glyphicon glyphicon-plus';
+    const FILTRAR = 'glyphicon glyphicon-filter';
+}
--- frontend/views/metas/index.php
+++ frontend/views/metas/index.php
@@ -1,7 +1,8 @@
 <?php
 
+use common\components\Icones;
 use yii\helpers\Html;
-use yii\grid\GridView;
+use kartik\grid\GridView;
 
 /* @var $this yii\web\View */
 /* @var $searchModel frontend\models\MetasSearch */
@@ -24,19 +25,97 @@ $this->params['breadcrumbs'][] = $this->title;
 
         <?= GridView::widget([
             'dataProvider' => $dataProvider,
+            'summary' => '',
+            'hover' => true,
+            'bordered' => false,
 //            'filterModel' => $searchModel,
             'columns' => [
-//                ['class' => 'yii\grid\SerialColumn'],
+                [
+                    'attribute' => 'idCategoria',
+                    'value' => function ($model) {
+                        /** @var \frontend\models\Categoria $nomeCategoria */
+                        $nomeCategoria = \frontend\models\Categoria::find()->where(['id' => $model->idCategoria])->one();
 
-//                'id',
-                'idCategoria',
+                        if (!empty($nomeCategoria)) {
+                            return $nomeCategoria->nome;
+                        }
+
+                        return null;
+                    }
+                ],
                 'descricao',
-                'valorPrevisto',
+                [
+                    'attribute' => 'valorPrevisto',
+//                    'format' => 'currency',
+
+                ],
                 'valorGasto',
                 'dataVencimento',
-                'status',
+                [
+                    'attribute' => 'status',
+                    'format' => 'html',
+                    'value' => function ($model) {
+                        $status = '';
+                        $labelColor = 'bg-green';
+                        $r = 'red';
 
-                ['class' => 'yii\grid\ActionColumn'],
+                        if ($model->status == 0) {
+                            $labelColor = 'bg-blue';
+                            $status = 'Pendente';
+                        } else if ($model->status == 1) {
+                            $labelColor = 'bg-gray';
+                            $status = 'Em andamento';
+                        } else {
+                            $status = 'Concluido';
+                        }
+
+                        $result = "<span class='point $labelColor' style='font-size: small'></span> $status";
+//                        $result = "<span class='point bg-danger'></span> $status ";
+                        return $result;
+                    }
+                ],
+
+                [
+                    'class' => '\yii\grid\ActionColumn',
+                    'header' => 'Ações',
+                    'headerOptions' => ['style' => 'color:#337ab7'],
+                    'template' => '{view} {update} {delete}',
+                    'buttons' => [
+                        'view' => function ($url, $model) {
+                            return Html::a('<span class="'. Icones::VISUALIZAR .'"> </span>', $url , [
+                                'title' => 'Exibir',
+//                            'onclick' => 'openModal("' . $url . '", false, true, "")',
+                                'class' => 'btn btn-default btn-sm',
+                            ]);
+                        },
+                        'update' => function ($url, $model) {
+                            return Html::a('<span class="'. Icones::EDITAR .'"> </span>', $url, [
+                                'class' => 'btn btn-warning btn-sm',
+                                'disabled' => true
+                            ]);
+                        },
+                        'delete' => function ($url, $model) {
+                            return Html::a('<span class="'. Icones::EXCLUIR .'"> </span>', $url, [
+                                'class' => 'btn btn-danger btn-sm',
+                                'title' => 'Apagar',
+                                'data-method' => 'post',
+                                'data-confirm' => Yii::t('yii', 'Confima a exclusão deste item?'),
+                            ]);
+                        },
+                        'urlCreator' => function ($action, $model) {
+                            if ($action === 'view') {
+                                return Url::to(['view', 'id' => $model->id]);
+
+                            }
+                            elseif ($action === 'update') {
+                                return Url::to(['update', 'id' => $model->id]);
+
+                            } elseif ($action === 'delete') {
+                                return Url::to(['delete', 'id' => $model->id]);
+                            }
+                        }
+                    ],
+                ],
             ],
         ]); ?>
     </div>
